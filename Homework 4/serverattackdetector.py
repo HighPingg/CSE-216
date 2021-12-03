5ac4eebf3e3b0197312870b6a58b55a4c8d7646e
Vincent Zheng <zhengvincent0825@gmail.com>
2021-12-02 21:57:21 -0500

    used datetime instead of direct string comparison allows for more accurate time operations if 1 second flips multiple times

--- Homework 4/serverattackdetector.py
+++ Homework 4/serverattackdetector.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 class ServerAttackDetector:
     def __init__(self, file: str):
         self.__file = file
@@ -31,32 +33,19 @@ class ServerAttackDetector:
         isSuspicious = curr[12].replace(" ", "") == "suspicious" and prev[12].replace(" ", "") == "suspicious"
         isSmallDuration = float(curr[1].replace(" ", "")) < 1
 
+        # Append '000' at the end of the time strings to turn into microseconds
+        prev[0] += "000"
+        curr[0] += "000"
+
         # We now need to check whether the previous attack is within a second of the current.
-        isQuick = True
-        prev = prev[0].split(" ")
-        curr = curr[0].split(" ")
+        # Create previous and current datetime instances
+        prev = datetime.strptime(prev[0], "%Y-%m-%d %H:%M:%S.%f")
+        curr = datetime.strptime(curr[0], "%Y-%m-%d %H:%M:%S.%f")
 
-        # We then split the datetimes into their date and time fields.
-        prevDate = prev[0]
-        prevTime = prev[1].split(":")
-
-        currDate = curr[0]
-        currTime = curr[1].split(":")
-
-        # This stack of if statements will check the dates to be the same. If they are not the same, then
-        # prevDate is over a day older than current.
-        # Check the years
-        if not(currDate == prevDate):
-            isQuick = False
-
-        # We can now check the times if the days are equal.
-        # Check if the hours and minutes are equal. If not, then they must not be more than a second long.
-        if isQuick and not(currTime[0] == prevTime[0] and currTime[1] == prevTime[1]):
-            isQuick = False
-        
-        # We can now check the seconds. We subtract the current seconds from the previous seconds
-        # and check that the difference is less than 1.
-        elif float(currTime[2]) - float(currTime[2]) > 1:
+        # Subtract times, check if difference is less than a second.
+        if (curr - prev).total_seconds() < 1:
+            isQuick = True
+        else:
             isQuick = False
 
         return isUDP and isSuspicious and isSmallDuration and isQuick
